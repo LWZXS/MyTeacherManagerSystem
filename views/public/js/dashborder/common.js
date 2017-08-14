5d0bcdc138d02843dfea3fdbc4394241fdf3ee73
xuelinWen <xuelin.work@gmail.com>
2017-08-14 21:56:56 +0800

    对项目进行模块化

---
+++ views/public/js/dashborder/common.js
@@ -0,0 +1,32 @@
+// NProgress.start();
+
+// NProgress.done();
+
+// $('.navs ul').prev('a').on('click', function () {
+// 	$(this).next().slideToggle();
+// });
+
+// if(!$.cookie("PHPSESSID") && location.pathname.indexOf('/login') == -1){
+// 	location.href = '/login';
+// }
+
+// if(location.pathname.indexOf('/login') == -1){
+// 	var tc_info = JSON.parse($.cookie("tc_info"));
+// 	var html = template("tc_profile",tc_info);
+// 	$(".aside>.profile").html(html);
+// 	// console.log(html);
+// }
+
+define(['jquery','cookie','template'], function ($,cookie,template) {
+	if (!$.cookie("PHPSESSID") && location.pathname.indexOf('/login') == -1) {
+		location.href = '/login';
+	}
+
+	if (location.pathname.indexOf('/login') == -1) {
+		var tc_info = JSON.parse($.cookie("tc_info"));
+		var html = template("tc_profile", tc_info);
+		$(".aside>.profile").html(html);
+		// console.log(html);
+	}
+
+});
