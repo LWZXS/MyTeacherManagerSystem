ce0e1a740a3cc6809467003d03dfd439aba5ee52
xuelinWen <xuelin.work@gmail.com>
2017-08-16 15:20:03 +0800

    完成了teacher_list页面点击编辑，跳转到add页面时候的渲染

--- views/public/js/teacher/manager.js
+++ views/public/js/teacher/manager.js
@@ -1,3 +1,30 @@
-define([], function () {
+define(['jquery', 'template'], function ($, template) {
     //点击编辑教师功能
+    // console.log(location.search);
+    // 根据跳转过来时候的链接后面的获取tc_id
+    var searchStr = location.search;
+    searchStr = searchStr.slice(1);
+    var searchArr = searchStr.split('&');
+    var objTemp = {};
+    for (var i = 0; i < searchArr.length; i++) {
+        var tempArr = searchArr[i].split('=');
+        objTemp[tempArr[0]] = tempArr[1];
+    }
+    $.ajax({
+        'url': '/api/teacher/edit',
+        type: 'get',
+        data: {
+            tc_id: objTemp.tc_id
+        },
+        success: function (res) {
+            // console.log(res);
+            if (res.code == 200) {
+                res.result.title = '讲师编辑';
+                res.result.saveBtnText = '保 存';
+                var htmlStr = template('tpl_teacherInfo_edit', res.result);
+                $('.main .teacher').html(htmlStr);
+                // console.log(htmlStr);
+            }
+        }
+    });
 });
